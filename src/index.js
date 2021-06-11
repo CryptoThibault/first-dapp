580e0782a6e5963127057e316777350dd70ab457
CryptoThibault <thibault.chalaoux@gmail.com>
2021-06-11 13:17:45 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
+  <React.StrictMode>
     <Web3Provider>
       <App />
-    </Web3Provider>,
+    </Web3Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 )
 
